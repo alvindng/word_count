f3cb246b0e3d6d5bef1629292d01b31efc2134e2
Alvin Ng <alvindng@gmail.com>
2016-07-01 10:15:40 -0700

    add the integration spec and tested successfully

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -2,3 +2,13 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('the user input path', {:type=> :feature}) do
+  it 'counts the number of occurences a word shows in the user submitted sentence' do
+    visit('/')
+    fill_in('word', :with => 'peck')
+    fill_in('sentence', :with => "If Peter Piper picked a peck of pickled peppers, where's the peck of pickled peppers Peter Piper picked?")
+    click_button("Let's see the Count!")
+    expect(page).to have_content("2")
+  end
+end
